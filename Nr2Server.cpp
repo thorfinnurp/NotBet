e4f0c5c007903f478cdb243df933d35d90fbc277
thorfinnurp <thorfinnurp@gmail.com>
2018-10-29 17:24:11 +0000

    LISTROUTES betur

--- Nr2Server.cpp
+++ Nr2Server.cpp
@@ -409,12 +409,12 @@ void echoMessage(char buffer[], int sender, int val, string username, int sockfd
         int RSPcounter = -1;
         size_t RSPpos = 0;
 
-        
-        string RSPtoken;
+        cout<<"RSPString"<< RSPString<<endl;
+        string RSPtoken = "";
         while ((RSPpos = RSPString.find(RSPdelimiter)) != string::npos) 
         {
             RSPtoken = RSPString.substr(0, RSPpos);
-
+            cout<<"RSPTOKEN:" << RSPtoken<< endl;
             clientsSockets[index].route.push_back(RSPtoken + "2");
         
             RSPString.erase(0, RSPpos + RSPdelimiter.length());
